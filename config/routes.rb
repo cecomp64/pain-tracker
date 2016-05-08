0df11ba059c9bc958b7a1f09b71e4cc948dab303
Carl Svensson <csvenss2@gmail.com>
2016-05-08 09:42:33 -0700

    Updating title, adding profile link.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :activities
-
   resources :pain_points
+  resources :users
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
