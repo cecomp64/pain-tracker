a5e8f32ee6aabc21eee34451c0ba13bcdc2282d2
Carl Svensson <csvenss2@gmail.com>
2016-11-27 09:58:40 -0800

    Fix Filter for min date max date

--- app/controllers/pain_points_controller.rb
+++ app/controllers/pain_points_controller.rb
@@ -10,10 +10,10 @@ class PainPointsController < ApplicationController
 
     # Try to fix timezones :(
     if(@filter[:min_date])
-      @filter[:min_date] = DateTime.strptime(@filter[:min_date]).change(offset: Time.current.zone)
+      @filter[:min_date] = DateTime.strptime(@filter[:min_date], '%m/%d/%Y %H:%M %p').change(offset: Time.current.zone)
     end
     if(@filter[:max_date])
-      @filter[:max_date] = DateTime.strptime(@filter[:max_date]).change(offset: Time.current.zone)
+      @filter[:max_date] = DateTime.strptime(@filter[:max_date], '%m/%d/%Y %H:%M %p').change(offset: Time.current.zone)
     end
 
 
